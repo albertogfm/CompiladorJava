0ac39ff80422d1edb7eec3c38c1d7264fd4c108b
HannaSophia <hscm.280400@gmail.com>
2020-11-07 16:51:07 -0600

    Borrar .class

---
+++ src/compilador/Main.java
@@ -0,0 +1,7 @@
+package compilador;
+import regex.*;
+public class Main{
+    public static void main(String args[]{
+        leerArchivo prueba = new leerArchivo("Hola haha"); //PRUEBAS
+        prueba.matcher("jmp inicio");//CADENA PRUEBA 
+}
--- src/regex/ArchivoRegex.java
+++ src/regex/ArchivoRegex.java
@@ -1,28 +1,34 @@
+package regex;
 import java.util.Scanner;
 import java.util.regex.*;
 
- class leerArchivo{
-    String nombre;
-    public leerArchivo(String nombreFile){
-        this.nombre=nombreFile;
-    }
+ class compilador{ //CLASE COMPILADOR
+     String convertidor(String operando){ //CONVERTIR LOS OPERANDOS A HEXADECIMAL
+         if (operando.charAt(0)=="$"){
+             //metodo para insertar en la lista (compilador) cuando ya estan en hexadecimal
+         }else //insertaremos el caso de cuando es decimal para convertirlo a hexadecimal y agregarlo al array compilador 
+
+     }
+
+ }
+ /*Método que se encarga de identificar el modo de direccionamiento a través de expresiones regulares y de validarlos*/
     void matcher(String cadena){
-        System.out.println("Entre");
-        Pattern expresionINH = Pattern.compile("^([a-zA-Z]{3,6})$");
-        Pattern expresionIMM = Pattern.compile("^([a-zA-Z]{3,6}( )(#)(\\$)?([0-9a-fA-F]{2,4}))$");
-        Pattern expresionEXT = Pattern.compile("^([a-zA-Z]{3,6}( )(\\$)?([0-9a-fA-F]{4}))$");
-        Pattern expresionDIR = Pattern.compile("^([a-zA-Z]{3,6}( )(\\$)?([0-9a-fA-F]{2}))$");
-        Pattern expresionREL = Pattern.compile("^([a-zA-Z]{3,6}( )[a-zA-Z]*)$");
-        Pattern expresionINDX = Pattern.compile("^([a-zA-Z]{3,6}( )(\\$)?([0-9a-fA-F]{2})(,[xX]))$");
+        //NOTAAAAAAAAAAA: AGREGAR EL FUNCIONAMIENTO EN ASSCCI PREGUNTA OBLIGATORIA EL LUNES 
+        Pattern expresionINH = Pattern.compile("^([a-zA-Z]{3,6})$"); // Identifica el direccionamiento INHERENTE
+        Pattern expresionIMM = Pattern.compile("^([a-zA-Z]{3,6}( )(#)(\\$)?([0-9a-fA-F]{2,4}))$");// Identifica a el irrcecionamiento INMEDiA
+        Pattern expresionEXT = Pattern.compile("^([a-zA-Z]{3,6}( )(\\$)?([0-9a-fA-F]{4}))$");// Identifica el dirrciconamiento EXTENDIDO
+        Pattern expresionDIR = Pattern.compile("^([a-zA-Z]{3,6}( )(\\$)?([0-9a-fA-F]{2}))$"); // Idntccfica el dirRELATIVOonamiento DIRECTO
+        Pattern expresionREL = Pattern.compile("^([a-zA-Z]{3,6}( )[a-zA-Z]*)$");// Identifica el dirr I/dentifica el dirrciconamientoe INDEXcionRESPECTO A "X"amiento IN/ Identifica el dirrecionamiento INHERENTE
+        Pattern expresionINDX = Pattern.compile("^([a-zA-Z]{3,6}( )(\\$)?([0-9a-fA-F]{2})(,[xX]))$");// Identifica el direccionamiento INDEXADO RESPECTO A "Y"
         Pattern expresionINDY = Pattern.compile("^([a-zA-Z]{3,6}( )(\\$)?([0-9a-fA-F]{2})(,[yY]))$");
-        
-        Matcher inh = expresionINH.matcher(cadena);
-        Matcher imm = expresionIMM.matcher(cadena);
-        Matcher ext = expresionEXT.matcher(cadena);
-        Matcher dir = expresionDIR.matcher(cadena);
-        Matcher rel = expresionREL.matcher(cadena);
-        Matcher indx = expresionINDX.matcher(cadena);
-        Matcher indy = expresionINDY.matcher(cadena);
+
+        Matcher  
+        Matcher imm = expresionIMM.matcher(cadena);//Valida la expresión EXTENDIDO
+        Matcher ext = expresionEXT.matcher(cadena);//Valida la expresión DIRECTO
+        Matcher dir = expresionDIR.matcher(cadena);//Valida la expresión RELATIVO
+        Matcher rel = expresionREL.matcher(cadena); //Valida la expresión INDEXADO RESPECTO A "X"        Matcher indx = expresionINDX.matcher(cadena);//Valida la expresión INDEXADO RESPECTO A "Y"
+        Matcher indy =//PRUEBAS expresionINDY.matcher(cadena);
+
         if(inh.find())
             System.out.println("Inherente");
         if(imm.find())
@@ -40,11 +46,4 @@ import java.util.regex.*;
 }
 }
 
-public class archivoRegex{
-    public static void main(String args[]){
-        System.out.println("A trabajar");
-        leerArchivo prueba = new leerArchivo("Hola haha");
-        prueba.matcher("jmp inicio");
-}
 
-}
