92d55afe96f0991bf976aba79fbd49f521ba52ae
Alberto <albertogarciafigueroam@gmail.com>
2020-11-07 18:22:21 -0600

    output errores

--- src/output/Output.java
+++ src/output/Output.java
@@ -4,4 +4,22 @@ import java.util.ArrayList;
 
 public class Output {
     public ArrayList<String> compilado = new ArrayList<>();
+    public ArrayList<Error> errores = new ArrayList<>();
+
+    public ArrayList<String> getCompilado() {
+        return compilado;
+    }
+
+    public void setCompilado(ArrayList<String> compilado) {
+        this.compilado = compilado;
+    }
+
+    public ArrayList<Error> getErrores() {
+        return errores;
+    }
+
+    public void setErrores(ArrayList<Error> errores) {
+        this.errores = errores;
+    }
+
 }
